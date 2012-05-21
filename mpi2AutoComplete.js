8f1a7c37b2913e9049019bd1e976a82bda1fbbed
Asfand Yar Qazi <aq2@sanger.ac.uk>
2012-05-21 16:32:52 +0100

    Get basic auto-complete working - very simple, needs modification

--- mpi2AutoComplete.js
+++ mpi2AutoComplete.js
@@ -10,20 +10,52 @@
 
         options: {
             source: function () {this.sourceCallback.apply(this, arguments);},
+            minLength: 1,
+            delay: 400,
+            solrURL: 'http://ikmc.vm.bytemark.co.uk:8983/solr/gene_autosuggest/select'
         },
 
         _create : function () {
             $.ui.autocomplete.prototype._create.apply(this);
         },
 
-        _setOption: function(key, value) {
-            $.Widget.prototype._setOption.apply(this, arguments);
+        _setOption: function (key, value) {
+            switch(key) {
+            case 'solrURL':
+                this.options.solrURL = value;
+                break;
+            }
+
+            $.ui.autocomplete.prototype._setOption.apply(this, arguments);
         },
 
-        sourceCallback: function(request, response) {
-            var term = request.term;
+        sourceCallback: function (request, response) {
+            var self = this;
+            var params = {
+                start: 0,
+                rows: 10,
+                q: request.term,
+                wt: 'json'
+            };
 
-            response(['Cbx1', 'Trafd1', 'Akt2-ps']);
+            params.q = params.q.replace(/[^A-Za-z0-9]/g, '');
+
+            $.ajax({
+                url: self.options.solrURL,
+                data: params,
+                dataType: 'jsonp',
+                jsonp: 'json.wrf',
+                timeout: 10000,
+                success: function (solrResponse) {
+                    var markerSymbols = $.map(solrResponse.response.docs, function (solrDoc) {
+                        return solrDoc.marker_symbol;
+                    });
+                    response(markerSymbols);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    response(['AJAX error']);
+                }
+            });
         }
     });
 
