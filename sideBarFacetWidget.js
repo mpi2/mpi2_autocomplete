734c71ee8572c78c1a35858a251829c306a5dce9
Chao-Kung Chen <ckchen@ebi.ac.uk>
2012-06-15 15:54:12 +0100

    geneFacet marker subtype

--- sideBarFacetWidget.js
+++ sideBarFacetWidget.js
@@ -85,9 +85,8 @@
 			thisCell.click(function(){
 				var geneGridId = self.options.geneGridId;
 				var marker_subType = $(this).attr('rel');
-				//var q = 'q=' + self.options.data.queryString + '&fq=marker_type:' + '"'+ marker_subType + '"';
-                var q = self.options.data.queryString + "&fq=marker_type:(protein coding gene)";
-                
+				              
+                var subTypeFilter = "marker_type:(protein coding gene)";
 				console.log('query: '+ q + ' id:'+ self.options.geneGridId + ' :login ' + self.options.isLoggedIn);
 				var callerElem = $('div#'+self.options.geneGridId);
 				callerElem.html('');
@@ -98,7 +97,7 @@
 					callerElem.mpi2Search({inputElements: false});
 				}
 				
- 				callerElem.trigger('search', [{q: q}]); 
+ 				callerElem.trigger('search', [{q: q, fq: subTypeFilter}]); 
 								
 			});
 		},
