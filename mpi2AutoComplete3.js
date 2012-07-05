f1de1025335e0c8d0e9375e0a6af862a9612f785
Chao-Kung Chen <ckchen@ebi.ac.uk>
2012-07-05 18:58:23 +0100

    test

--- mpi2AutoComplete3.js
+++ mpi2AutoComplete3.js
@@ -109,11 +109,10 @@
 						
 						var pathname = window.location.pathname;
 						if ( pathname.indexOf('/search-widget-prototype-3') == -1 ){
-							var params = "?type=" + self._setSearchMode() + "&solrParams=" + self.term + "&geneFound=" + self.options.geneFound;
+							//var params = "?type=" + self._setSearchMode() + "&solrParams=" + self.term + "&geneFound=" + self.options.geneFound;
 							var params = {type: self._setSearchMode(), solrParams: self.term, geneFound: self.options.geneFound};
 							$('div#hidden_search_params').html(params);
-							window.location.replace("http://dev.mousephenotype.org/search-widget-prototype-3");
-							console.log('new');
+							window.location.replace("http://dev.mousephenotype.org/search-widget-prototype-3");							
 						}
 						else {
 							$('div#hidden_search_params').html('');
